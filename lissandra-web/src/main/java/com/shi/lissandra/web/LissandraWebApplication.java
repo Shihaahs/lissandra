505dbb7556a21c1b3d4d645a296cd267faa5ca56
WuEr <wuer@maihaoche.com>
2019-01-15 14:53:09 +0800

    init项目Lissandra

---
+++ lissandra-web/src/main/java/com/shi/lissandra/web/LissandraWebApplication.java
@@ -0,0 +1,14 @@
+package com.shi.lissandra.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LissandraWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LissandraWebApplication.class, args);
+    }
+
+}
+
