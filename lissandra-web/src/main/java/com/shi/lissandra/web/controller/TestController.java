fd053c3e30f34c5f8258c6a5dc8ab9615ce1d394
WuEr <wuer@maihaoche.com>
2019-03-28 11:39:16 +0800

    fix

--- lissandra-web/src/main/java/com/shi/lissandra/web/controller/TestController.java
+++ lissandra-web/src/main/java/com/shi/lissandra/web/controller/TestController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date: 2019/1/30 2:07 PM
  * @Copyright: 2017-2020 www.maihaoche.com Inc. All rights reserved.
  * 注意：本内容仅限于卖好车内部传阅，禁止外泄以及用于其他的商业目
- * @Description:
+ * @Description: 测试网关
  */
 
 @Slf4j
