348df08136aa0fb37b449f3a98cec932f53f64ad
wuer <1419903014@qq.com>
2019-05-26 14:24:40 +0800

    fix

--- lissandra-web/src/main/java/com/shi/lissandra/web/LissandraWebApplication.java
+++ lissandra-web/src/main/java/com/shi/lissandra/web/LissandraWebApplication.java
@@ -1,13 +1,9 @@
 package com.shi.lissandra.web;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
